bcd56d2cec59fb971ae6deb48bc448816ac3052d
ShabanaParven <92175723+ShabanaParven@users.noreply.github.com>
2021-10-13 18:32:49 +0530

    about how to creat array and find the lenght of array

---
+++ class_work/array.sh
@@ -0,0 +1,32 @@
+#! /bin/sh -x
+
+#num[0]=10
+
+#num[1]=20
+
+#num[2]=30
+
+#num[3]=40
+
+#num[4]=50
+
+#echo ${num[*]}
+
+#echo ${num[2]}
+
+#echo ${num[7]}
+
+#echo ${num[@]:2}
+
+#echo ${num[@]: 1 : 3}
+
+
+#length of array
+
+#numbers=(1 2 3 4 5 6 7 8 9 10)
+
+echo "length : ${#numbers[@]}"
+
+name=("shabana" "parveen" "iman" "maira")
+
+echo  "length of perticular index: ${#name[2]}"
