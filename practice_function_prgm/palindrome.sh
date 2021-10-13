5ff024012ad3e66c6177294b7fd6ec4e9a39c115
ShabanaParven <92175723+ShabanaParven@users.noreply.github.com>
2021-10-13 17:52:25 +0530

    program to find given number is palindrome or not

---
+++ practice_function_prgm/palindrome.sh
@@ -0,0 +1,24 @@
+#! /bin/sh -x
+
+read -p "enter the number: " n
+function pal
+{
+number=$n
+reverse=0
+
+while [ $n -gt 0 ]
+do
+a=`expr $n % 10`
+n=`expr $n / 10`
+reverse=`expr $reverse \* 10 + $a`
+done
+echo $reverse
+if [ $number -eq $reverse ]
+then
+echo "number is palindrome"
+else
+echo "number is not a palindrome"
+fi
+}
+r=`pal $n`
+echo "$r"
