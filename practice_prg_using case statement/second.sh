daf9b193d7e155536e711cd291a67d43f6d464da
ShabanaParven <92175723+ShabanaParven@users.noreply.github.com>
2021-10-13 17:34:22 +0530

    programs using case statements

---
+++ practice_prg_using case statement/second.sh
@@ -0,0 +1,13 @@
+read -p "enter a single digit number" n
+
+case $n in
+
+1 )echo "sunday" ;;
+2 )echo "monday" ;;
+3 )echo "tuesday" ;;
+4 )echo "wednesay" ;;
+5 )echo "thursday" ;;
+6 )echo "friday" ;;
+7 )echo "saturday" ;;
+
+esac
